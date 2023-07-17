0d4eacd0369517a539cc9bc4f3c75d0fc60ebe41
Tamai1306 <ramirezgordillotamai@gmail.com>
2023-07-17 17:28:08 +0200

    Final

--- TFM/launch.sh
+++ TFM/launch.sh
@@ -4,7 +4,7 @@ export containerName=Trabajo_Fin_Master
 sleep 3 && \
         xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerName` >/dev/null 2>&1 &
 docker run -d --gpus '"device=0"' --rm -it \
-	--volume="/home/tamai/Desktop/TFM:/workspace:rw" \
+	--volume="/home/tamai/Desktop/TFM_huggingface:/workspace:rw" \
 	--volume=$HOME/.Xauthority:/root/.Xauthority:ro \
 	--workdir="/workspace" \
 	-v $XSOCK:$XSOCK:rw \
